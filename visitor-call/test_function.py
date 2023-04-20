f2bf10f6b9e614fca975f470bd012d25f0420177
Boatstimu <116456849+Boatstimu@users.noreply.github.com>
2023-04-20 18:42:26 -0500

    Adding lambda unit test

---
+++ visitor-call/test_function.py
@@ -0,0 +1,8 @@
+import unittest
+
+target = __import__("function.py")
+handler = target.handler
+
+class TestLamba(unittest.TestCase):
+    def test_handler(self):
+        self.assertEqual(handler(("status","test")["statusCode"],200))
