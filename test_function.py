a3494e668bd07e4b8ae4d760bf83173800daa12b
Boatstimu <116456849+Boatstimu@users.noreply.github.com>
2023-04-20 23:44:52 -0500

    Removing extra test function

--- test_function.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from 
-
-
-class TestLamba(unittest.TestCase):
-    def test_handler(self):
-        print("Running unit test...")
-        self.assertEqual(handler("status","test")["statusCode"],200)
