a6eb0a51a7cac953353ec76bf55de13eff14d3d8
Boatstimu <116456849+Boatstimu@users.noreply.github.com>
2023-04-22 18:20:57 -0500

    Updating unit test

--- visitor-call/test_function.py
+++ visitor-call/test_function.py
@@ -7,3 +7,7 @@ class TestLamba(unittest.TestCase):
     def test_handler(self):
         print("Running unit test...")
         self.assertEqual(handler("status","test")["statusCode"],200)
+
+if __name__ == '__main__':
+    unittest.main()
+      
