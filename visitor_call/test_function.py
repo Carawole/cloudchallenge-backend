90040a72b744002fbcf43501cd189bc463c14704
Boatstimu <116456849+Boatstimu@users.noreply.github.com>
2023-04-23 21:34:34 -0500

    Editing unit test

--- visitor_call/test_function.py
+++ visitor_call/test_function.py
@@ -1,20 +1,21 @@
 import unittest
 import json
-import boto3
-from moto import mock_dynamodb
+from visitor_call import handler
+#import boto3
+#from contextlib import contextmanager
 
-"""
+
 class test_lambda(unittest.TestCase):
     def test_handler(self):
         print("Running unit test...")
         self.assertEqual(handler("status","test")["statusCode"],200)
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
 """
 class test_lambda(unittest.TestCase):
     @mock_dynamodb
-    def test_get_from_dynamo(self):
+    def test_get_from_dynamo(dynamo):
         from visitor_call import function
         handler = function.handler
         dynamo = boto3.resource('dynamodb', region_name='us-east-1')
@@ -34,3 +35,4 @@ class test_lambda(unittest.TestCase):
         #assert status_Code == 200
         self.assertEqual(handler("status","test")["statusCode"],200)
 
+"""
